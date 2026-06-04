ba0d789de8c84017577d5c1f0a1a68fc3a1d3bb7
ribanez-design <ribanez@t2b.tech>
2026-06-04 18:21:44 +0200

    Add files via upload
    Signed-off-by: ribanez-design <ribanez@t2b.tech>

--- NEXTCLOUD.cmake
+++ NEXTCLOUD.cmake
@@ -5,38 +5,33 @@
 # keep the application name and short name the same or different for dev and prod build
 # or some migration logic will behave differently for each build
 if(NEXTCLOUD_DEV)
-    set( APPLICATION_NAME       "NextcloudDev" )
-    set( APPLICATION_SHORTNAME  "NextcloudDev" )
-    set( APPLICATION_EXECUTABLE "nextclouddev" )
-    set( APPLICATION_ICON_NAME  "Nextcloud" )
+    set( APPLICATION_NAME       "T2BDriveDev" )
+    set( APPLICATION_SHORTNAME  "T2BDriveDev" )
+    set( APPLICATION_EXECUTABLE "t2bdrivedev" )
+    set( APPLICATION_ICON_NAME  "T2BDrive" )
 else()
-    set( APPLICATION_NAME       "Nextcloud" )
-    set( APPLICATION_SHORTNAME  "Nextcloud" )
-    set( APPLICATION_EXECUTABLE "nextcloud" )
-    set( APPLICATION_ICON_NAME  "${APPLICATION_SHORTNAME}" )
+    set( APPLICATION_NAME       "T2B Drive" )
+    set( APPLICATION_SHORTNAME  "T2BDrive" )
+    set( APPLICATION_EXECUTABLE "t2bdrive" )
+    set( APPLICATION_ICON_NAME  "T2BDrive" )
 endif()
 
 set( APPLICATION_CONFIG_NAME "${APPLICATION_EXECUTABLE}" )
-set( APPLICATION_DOMAIN     "nextcloud.com" )
-set( APPLICATION_VENDOR     "Nextcloud GmbH" )
-set( APPLICATION_UPDATE_URL "https://updates.nextcloud.org/client/" CACHE STRING "URL for updater" )
+set( APPLICATION_DOMAIN     "t2b.tech" )
+set( APPLICATION_VENDOR     "Tech2Business" )
+set( APPLICATION_UPDATE_URL "" CACHE STRING "URL for updater" )
 set( APPLICATION_HELP_URL   "" CACHE STRING "URL for the help menu" )
 
-if(APPLE AND (APPLICATION_NAME STREQUAL "Nextcloud" OR NEXTCLOUD_DEV) AND EXISTS "${CMAKE_SOURCE_DIR}/theme/colored/Nextcloud-macOS-icon.svg")
-    set( APPLICATION_ICON_NAME "Nextcloud-macOS" )
-    message("Using macOS-specific application icon: ${APPLICATION_ICON_NAME}")
-endif()
-
-set( APPLICATION_ICON_SET   "SVG" )
-set( APPLICATION_SERVER_URL "" CACHE STRING "URL for the server to use. If entered, the UI field will be pre-filled with it" )
+set( APPLICATION_ICON_SET   "PNG" )
+set( APPLICATION_SERVER_URL "http://t2bdrive.local:30027" CACHE STRING "URL for the server to use. If entered, the UI field will be pre-filled with it" )
 set( APPLICATION_SERVER_URL_ENFORCE ON ) # If set and APPLICATION_SERVER_URL is defined, the server can only connect to the pre-defined URL
-set( APPLICATION_REV_DOMAIN "com.nextcloud.desktopclient" )
-set( DEVELOPMENT_TEAM "NKUJUXUJ3B" CACHE STRING "Apple Development Team ID" )
-set( APPLICATION_VIRTUALFILE_SUFFIX "nextcloud" CACHE STRING "Virtual file suffix (not including the .)")
+set( APPLICATION_REV_DOMAIN "tech.t2b.drive" )
+set( DEVELOPMENT_TEAM "" CACHE STRING "Apple Development Team ID" )
+set( APPLICATION_VIRTUALFILE_SUFFIX "t2bdrive" CACHE STRING "Virtual file suffix (not including the .)")
 set( APPLICATION_OCSP_STAPLING_ENABLED OFF )
 set( APPLICATION_FORBID_BAD_SSL OFF )
 
-set( LINUX_PACKAGE_SHORTNAME "nextcloud" )
+set( LINUX_PACKAGE_SHORTNAME "t2bdrive" )
 set( LINUX_APPLICATION_ID "${APPLICATION_REV_DOMAIN}.${LINUX_PACKAGE_SHORTNAME}")
 
 set( THEME_CLASS            "NextcloudTheme" )
@@ -44,26 +39,23 @@ set( WIN_SETUP_BITMAP_PATH  "${CMAKE_SOURCE_DIR}/admin/win/nsi" )
 
 set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-background.png" CACHE STRING "The MacOSX installer background image")
 
-# set( THEME_INCLUDE          "${OEM_THEME_DIR}/mytheme.h" )
-# set( APPLICATION_LICENSE    "${OEM_THEME_DIR}/license.txt )
-
 ## Updater options
-option( BUILD_UPDATER "Build updater" ON )
+option( BUILD_UPDATER "Build updater" OFF )
 
-option( WITH_PROVIDERS "Build with providers list" ON )
+option( WITH_PROVIDERS "Build with providers list" OFF )
 
 option( ENFORCE_VIRTUAL_FILES_SYNC_FOLDER "Enforce use of virtual files sync folder when available" OFF )
 option( DISABLE_VIRTUAL_FILES_SYNC_FOLDER "Disable use of virtual files sync folder even when available" OFF )
 
-option(ENFORCE_SINGLE_ACCOUNT "Enforce use of a single account in desktop client" OFF)
+option(ENFORCE_SINGLE_ACCOUNT "Enforce use of a single account in desktop client" ON)
 
 option( DO_NOT_USE_PROXY "Do not use system wide proxy, instead always do a direct connection to server" OFF )
 
 option( WIN_DISABLE_USERNAME_PREFILL "Do not prefill the Windows user name when creating a new account" OFF )
 
 ## Theming options
-set(NEXTCLOUD_BACKGROUND_COLOR "#0082c9" CACHE STRING "Default Nextcloud background color")
-set( APPLICATION_WIZARD_HEADER_BACKGROUND_COLOR ${NEXTCLOUD_BACKGROUND_COLOR} CACHE STRING "Hex color of the wizard header background")
+set(NEXTCLOUD_BACKGROUND_COLOR "#E87722" CACHE STRING "Default background color")
+set( APPLICATION_WIZARD_HEADER_BACKGROUND_COLOR "#E87722" CACHE STRING "Hex color of the wizard header background")
 set( APPLICATION_WIZARD_HEADER_TITLE_COLOR "#ffffff" CACHE STRING "Hex color of the text in the wizard header")
 option( APPLICATION_WIZARD_USE_CUSTOM_LOGO "Use the logo from ':/client/theme/colored/wizard_logo.(png|svg)' else the default application icon is used" ON )
 
@@ -72,17 +64,17 @@ option( APPLICATION_WIZARD_USE_CUSTOM_LOGO "Use the logo from ':/client/theme/co
 #
 if(WIN32)
     # Context Menu
-    set( WIN_SHELLEXT_CONTEXT_MENU_GUID      "{BC6988AB-ACE2-4B81-84DC-DC34F9B24401}" )
+    set( WIN_SHELLEXT_CONTEXT_MENU_GUID      "{A1342B74-7593-4C70-9D61-22F294AAFE05}" )
 
     # Overlays
-    set( WIN_SHELLEXT_OVERLAY_GUID_ERROR     "{E0342B74-7593-4C70-9D61-22F294AAFE05}" )
-    set( WIN_SHELLEXT_OVERLAY_GUID_OK        "{E1094E94-BE93-4EA2-9639-8475C68F3886}" )
-    set( WIN_SHELLEXT_OVERLAY_GUID_OK_SHARED "{E243AD85-F71B-496B-B17E-B8091CBE93D2}" )
-    set( WIN_SHELLEXT_OVERLAY_GUID_SYNC      "{E3D6DB20-1D83-4829-B5C9-941B31C0C35A}" )
-    set( WIN_SHELLEXT_OVERLAY_GUID_WARNING   "{E4977F33-F93A-4A0A-9D3C-83DEA0EE8483}" )
+    set( WIN_SHELLEXT_OVERLAY_GUID_ERROR     "{A0342B74-7593-4C70-9D61-22F294AAFE05}" )
+    set( WIN_SHELLEXT_OVERLAY_GUID_OK        "{A1094E94-BE93-4EA2-9639-8475C68F3886}" )
+    set( WIN_SHELLEXT_OVERLAY_GUID_OK_SHARED "{A243AD85-F71B-496B-B17E-B8091CBE93D2}" )
+    set( WIN_SHELLEXT_OVERLAY_GUID_SYNC      "{A3D6DB20-1D83-4829-B5C9-941B31C0C35A}" )
+    set( WIN_SHELLEXT_OVERLAY_GUID_WARNING   "{A4977F33-F93A-4A0A-9D3C-83DEA0EE8483}" )
 
     # MSI Upgrade Code (without brackets)
-    set( WIN_MSI_UPGRADE_CODE                "FD2FCCA9-BB8F-4485-8F70-A0621B84A7F4" )
+    set( WIN_MSI_UPGRADE_CODE                "AD2FCCA9-BB8F-4485-8F70-A0621B84A7F4" )
 
     # Windows build options
     option( BUILD_WIN_MSI "Build MSI scripts and helper DLL" OFF )
